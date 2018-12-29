33c94533d4c9d94b52ea717cabc6ec64eb9a321e
Pierre <riandeypierre@gmail.Com>
2018-12-29 11:48:44 +0100

    reorganized code

--- db_feed.py
+++ db_feed.py
@@ -16,17 +16,14 @@ cursor = cnx.cursor()
 page = 1
 #request_scope = int(input("Number of page you wish to ask (100 entry/page) :"))
 #request_scope += 1
-while page < 2:
+try:
+    print("Looking for cached data")
+    with open("resources/off_local_file.json", "r") as file:
+        product_file = json.load(file)
 
-    str_page = str(page)
-    page += 1
-
-    try:
-        print("Looking for cached data")
-        with open("resources/off_local_file.json", "r") as file:
-            product_file = json.load(file)
-
-    except FileNotFoundError:
+except FileNotFoundError:
+    while page < 5:
+        str_page = str(page)
         print("Requesting page {}...".format(page))
         payload = {'action':'process',
                    'tagtype_0':'languages',
@@ -35,8 +32,8 @@ while page < 2:
                    'sort_by':'unique_scans_n',
                    'json':'1',
                    'page_size':'1000',
-                   'page': '2'}
-
+                   'page': str_page}
+        page += 1
         brands = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=payload)
         json_file = brands.json()
 
@@ -48,19 +45,19 @@ while page < 2:
             product_file = json.load(file)
         print("Request output saved to file")
 
-    for product in product_file['products']:
-        try:
-            add_product = ("INSERT INTO product "
-                           "(product_name,nutrition_grade, product_category, product_url)"
-                           "VALUES (%s, %s, %s, %s)")
+for product in product_file['products']:
+    try:
+        add_product = ("INSERT INTO product "
+                       "(product_name,nutrition_grade, product_category, product_url)"
+                       "VALUES (%s, %s, %s, %s)")
 
-            data_product = (product['product_name'], product['nutrition_grades'], product['categories_tags'][0][3:], product['url'])
-            print("{} ({}), cat:{}, url: {}".format(product['product_name'], product['nutrition_grades'], product['categories_tags'][0][3:], product['url']))
-            cursor.execute(add_product,data_product)
-            cnx.commit()
-        except KeyError:
-            print("KeyError")
-            continue
+        data_product = (product['product_name'], product['nutrition_grades'], product['categories_tags'][0][3:], product['url'])
+        print("{} ({}), cat:{}, url: {}".format(product['product_name'], product['nutrition_grades'], product['categories_tags'][0][3:], product['url']))
+        cursor.execute(add_product,data_product)
+        cnx.commit()
+    except KeyError:
+        print("KeyError")
+        continue
 
 cursor.close()
 cnx.close()
